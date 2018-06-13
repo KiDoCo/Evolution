0a99ff8a5063deab42fb79943c84fc33d4dba9d4
Huhtala Henri TTK16SP <henrihuhtala@kamk.fi>
2018-06-13 15:56:10 +0300

    kalojen AI juttuja

---
+++ Assets/FishController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishController : MonoBehaviour {
+
+    public PathManager pathManager;
+    private Node node;
+
+    public LayerMask threatMask;
+    private LayerMask obstacleMask;
+
+    private enum States {roam, chased};
+    States curState = States.roam;
+
+    Collider mCollider;
+    Vector3 boxScale;
+
+    void Start()
+    {
+        node = pathManager.GetClosestNode(transform.position);
+        mCollider = gameObject.GetComponent<Collider>();
+        boxScale = transform.localScale;
+        boxScale.z *= 2;
+        obstacleMask = pathManager.obstacleLayer;
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        switch (curState)
+        {
+            case States.roam:
+                if (Vector3.Distance(gameObject.transform.position, node.position) > 0.1f)
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, node.position, pathManager.speed * Time.deltaTime);
+                    Physics.BoxCast(mCollider.bounds.center, boxScale * 2, transform.forward, out hit, transform.rotation, 1, threatMask);
+                    if (hit.transform)
+                    {
+                        CheckVisible(hit.transform);
+                    }
+                }
+                else
+                {
+                    node = pathManager.GetNextNode(transform.position, node);
+                    transform.LookAt(node.position);
+                }
+                break;
+            case States.chased:
+                transform.Translate(Vector3.forward * pathManager.escapeSpeed * Time.deltaTime);
+                Physics.BoxCast(mCollider.bounds.center, boxScale * 1.5f, transform.forward, out hit, transform.rotation, 1, obstacleMask);
+                if (hit.transform)
+                {
+                    transform.Rotate(Vector3.up*200*Time.deltaTime);
+                }
+                /*
+                if (Physics.SphereCast(transform.position, 1.5f, transform.forward,out hit, 1, obstacleMask))
+                {
+                    transform.Rotate(Vector3.up*200*Time.deltaTime);
+                }*/
+                Debug.DrawRay(transform.position, transform.forward*1.5f, Color.green);
+                break;
+        }
+    }
+
+    private void CheckVisible(Transform threat)
+    {
+        if (!Physics.Linecast(transform.position, threat.position, obstacleMask))
+        {
+            transform.LookAt(threat.position);
+            transform.Rotate(Vector3.up * 180);
+            curState = States.chased;
+            Invoke("SetRoam", 10);
+        }
+    }
+
+    private void SetRoam()
+    {
+        node = pathManager.GetClosestNode(transform.position);
+        curState = States.roam;
+    }
+}
---
+++ Assets/PathManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathManager : MonoBehaviour {
+
+    public LayerMask obstacleLayer;
+    public List<Node> allNodes = new List<Node>();
+    Node defNode, bestNode;
+
+    public float speed;
+    public float escapeSpeed;
+
+
+    void Awake()
+    {
+        defNode = new Node(new Vector3(), this);
+        for (int i = 0; i < gameObject.transform.childCount; i++)
+        {
+            allNodes.Add(new Node(gameObject.transform.GetChild(i).position, this));
+            Destroy(gameObject.transform.GetChild(i).gameObject);
+        }
+        foreach (Node n in allNodes)
+        {
+            foreach (Node nn in allNodes)
+            {
+                if (n != nn && !Physics.Linecast(n.position, nn.position, obstacleLayer))
+                {
+                    Debug.DrawLine(n.position, nn.position, Color.red, 100);
+                    n.neighbors.Add(nn);
+                }
+
+            }
+        }
+    }
+
+
+    public Node GetClosestNode(Vector3 startPos)
+    {
+        float tempDist = 100;
+        foreach (Node n in allNodes)
+        {
+            if (!Physics.Linecast(startPos, n.position, obstacleLayer) && Vector3.Distance(startPos, n.position) < tempDist)
+            {
+                bestNode = n;
+                tempDist = Vector3.Distance(startPos, n.position);
+            }
+        }
+        return bestNode;
+    }
+
+    public Node GetNextNode(Vector3 startPos, Node prevNode)
+    {
+        return prevNode.neighbors[Random.Range(0, prevNode.neighbors.Count)];
+    }
+}
