c4518e3bdb8eb169f6f052d7498c43013f7dd87d
Huhtala Henri TTK16SP <henrihuhtala@kamk.fi>
2018-06-20 12:47:21 +0300

    hook

---
+++ Assets/AI Roskaa/HookAbility.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookAbility : MonoBehaviour {
+
+
+    private GameObject hookObj;
+    public GameObject hookPrefab;
+    [SerializeField] private float hookDistance;
+    [SerializeField] private float hookSpeed;
+    [SerializeField] private float slowDown;
+    private bool hookShot = false;
+
+    
+
+    void Start ()
+    {
+		
+	}
+	
+
+
+	void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.E))
+        {
+            ShootHook();
+        }
+	}
+
+    void ShootHook()
+    {
+        if (!hookShot)
+        {
+            hookObj = Instantiate(hookPrefab, transform.position, transform.rotation);
+            hookObj.GetComponent<Hook>().SetValues(gameObject, transform.forward, hookSpeed);
+            Invoke("ResetHook", hookDistance);
+            hookShot = true;
+        }
+    }
+
+    void ResetHook()
+    {
+        if (!hookObj.GetComponent<Hook>().hitTarget)
+        {
+            Destroy(hookObj);
+            hookShot = false;
+        }
+    }
+
+}
