d11ab5b30c249939ed3f4145ebd9ee62cfb636cc
Tuominen Joonas TTK17SP <joonastuominen@kamk.fi>
2018-06-26 10:44:50 +0300

    ADD Gamemanager starts matches

--- Assets/Scripts/Managers/Gamemanager.cs
+++ Assets/Scripts/Managers/Gamemanager.cs
@@ -7,29 +7,30 @@ using System.IO;
 
 
 public class Gamemanager : NetworkBehaviour
-{
-    #pragma warning disable
-    public static Gamemanager Instance;
+{ //Ingnore pragmas for unnecessary warnings
+#pragma warning disable
+    public static Gamemanager           Instance;
     //File location variables hard coded shishnet
-    private string hPFileLocation = "/Assets/StreamingAssets/AssetBundles/carnivore.pl"; //herbivore asset location
-    private string cPFileLocation = "/Assets/StreamingAssets/AssetBundles/herbivore.pl"; //carnivore asset location
-   
+    private string                      hPFileLocation = "/Assets/StreamingAssets/AssetBundles/carnivore.pl"; //herbivore asset location
+    private string                      cPFileLocation = "/Assets/StreamingAssets/AssetBundles/herbivore.pl"; //carnivore asset location
+
     //Match variables
+    private const float                 startingMatchTimer = 5.0f; //time value in minutes
+    private const float                 minutesToSeconds = 60.0f;  //converting value
+    private const float                 interval = 2.0f;           //The time in seconds that spawning will happen
     [SyncVar]
     private float                       matchTimer;
-    private const float                 startingMatchTimer = 5.0f; //time value in minutes
-    private const float                 minutesToSeconds   = 60.0f;
-    private const float                 interval           = 2.0f;
+    private bool                        MatchStarting;
 
     //Gamemanager lists
-    private Dictionary<int,GameObject>  carnivorePrefabs     = new Dictionary<int, GameObject>();
+    private Dictionary<int, GameObject> carnivorePrefabs     = new Dictionary<int, GameObject>();
     private Dictionary<int, GameObject> herbivorePrefabs     = new Dictionary<int, GameObject>();
-    private List<GameObject>            foodsources          = new List<GameObject>();
-    public  List<IEatable>              FoodPlaceList        = new List<IEatable>();
+    public List<GameObject>             foodsources          = new List<GameObject>();
+    public List<IEatable>               FoodPlaceList        = new List<IEatable>();
     private List<Transform>             FoodSpawnPointList   = new List<Transform>();
     private List<Transform>             PlayerSpawnPointList = new List<Transform>();
-    public  List<Test>                  PlayerList           = new List<Test>();
-    #pragma warning restore
+    public List<Test>                   PlayerList           = new List<Test>();
+#pragma warning restore
 
     public float MatchTimer
     {
@@ -45,7 +46,7 @@ public class Gamemanager : NetworkBehaviour
     }
 
     /// <summary>
-    /// periodically increases the foodamount to every object that implements IEtable interface
+    /// periodically increases the foodamount 
     /// </summary>
     private void IncreaseFoodOnSources()
     {
@@ -53,22 +54,24 @@ public class Gamemanager : NetworkBehaviour
     }
 
     /// <summary>
-    /// Starts the match between player
+    /// Starts the match between players. Must be called after loading the game scene
     /// </summary>
-    public void StartMatch()
+    public IEnumerator StartMatch()
     {
+        if (MatchStarting) yield break;
+        MatchStarting = true;
+
         //check the player selection and add them to a list
-        for(int i = 0; i < FoodSpawnPointList.Capacity; i++)
+        for (int i = 0; i < FoodSpawnPointList.Capacity; i++)
         {
-        //playerselection blah blah 
-        //playerlist.add(playerselection[i])
+            //playerselection blah blah 
+            //playerlist.add(playerselection[i])
         }
+        //Stop for a moment to scene to load
+        yield return new WaitForSeconds(1.0f);
 
-        //Load the match scene
-        SceneManager.LoadScene("Game");
-
-        //search every spawnpoint
-        for (int i = 0; i < GameObject.FindObjectsOfType<IEatable>().Length; i++)
+        //search every spawnpoint for players and foodsources
+        for (int i = 0; i < GameObject.FindGameObjectsWithTag("FoodSource").Length; i++)
         {
             FoodSpawnPointList.Add(GameObject.FindGameObjectsWithTag("FoodSource")[i].transform);
         }
@@ -80,13 +83,14 @@ public class Gamemanager : NetworkBehaviour
 
         //set the match timer and spawn the objects
         MatchTimer = startingMatchTimer * minutesToSeconds;
-        Debug.Log("foodspawnlist count" + FoodSpawnPointList.Count);
-        Debug.Log("playerspawnlist count" + PlayerSpawnPointList.Count);
         SpawnPlayers();
         SpawnFoodSources();
-        //repeaters
+
+        //repeaters for spawning food/populating sources
         InvokeRepeating("IncreaseFoodOnSources", interval, interval);
         InvokeRepeating("SpawnFish", interval, interval);
+        MatchStarting = false;
+        yield return matchTimer;
     }
 
     /// <summary>
@@ -94,11 +98,14 @@ public class Gamemanager : NetworkBehaviour
     /// </summary>
     public void EndMatch()
     {
-        //stop players movement
-        //match result for remaining players
-        //insert function to kill server after x seconds 
-        //and return remaining players to lobby/menu screen
+        /*
+        stop players movement
+        match result for remaining players
+        insert function to kill server after x seconds 
+        and return remaining players to lobby/menu screen
+        */
         CancelInvoke();
+        //killserver
     }
 
     /// <summary>
@@ -114,9 +121,9 @@ public class Gamemanager : NetworkBehaviour
     /// </summary>
     public void SpawnPlayers()
     {
-        GameObject clone = Instantiate(carnivorePrefabs[0], PlayerSpawnPointList[0]);
-        for(int i = 0; i < PlayerList.Capacity; i++)
+        for (int i = 0; i < PlayerList.Capacity; i++)
         {
+        GameObject clone = Instantiate(carnivorePrefabs[0], PlayerSpawnPointList[0].position, Quaternion.identity);
             //list of player and loop it to every individual player
             clone.name = "Player";
         }
@@ -130,14 +137,11 @@ public class Gamemanager : NetworkBehaviour
         //Insert function to spawn a fish at a random spawnpoint
     }
 
-    /// <summary>
-    /// Populates the food sources
-    /// </summary>
     private void SpawnFoodSources()
     {
-        for(int i = 0; i < FoodSpawnPointList.Capacity; i++)
+        for (int i = 0; i < FoodSpawnPointList.Capacity; i++)
         {
-            GameObject clone = Instantiate(foodsources[0], FoodSpawnPointList[i], false);
+            GameObject clone = Instantiate(foodsources[0], FoodSpawnPointList[i].position, Quaternion.identity);
             clone.name = "FoodSource " + i;
         }
     }
@@ -154,28 +158,38 @@ public class Gamemanager : NetworkBehaviour
 
         for (int i = 0; i < Ctemp.Count; i++)
         {
-            carnivorePrefabs.Add(i ,Ctemp[i]);
+            carnivorePrefabs.Add(i, Ctemp[i]);
         }
 
         for (int i = 0; i < Htemp.Count; i++)
         {
-            herbivorePrefabs.Add(i,Htemp[i]);
+            herbivorePrefabs.Add(i, Htemp[i]);
         }
 
         Debug.Log("Carnivores loaded: " + carnivorePrefabs.Count);
         Debug.Log("Herbivores loaded: " + herbivorePrefabs.Count);
     }
 
+    public void LoadGame()
+    {
+        //Load the match scene
+        SceneManager.LoadSceneAsync("Game");
+        StartCoroutine(StartMatch());
+    }
+
     //unity methods
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.F2))
+        if (SceneManager.GetActiveScene().name != "Game")
         {
-            EventManager.Broadcast(EVENT.RoundBegin);
+            if (Input.GetKey(KeyCode.F2))
+            {
+                EventManager.Broadcast(EVENT.RoundBegin);
+            }
         }
 
-        if(MatchTimer <= 0)
+        if (MatchTimer <= 0)
         {
             EndMatch();
         }
@@ -189,6 +203,6 @@ public class Gamemanager : NetworkBehaviour
     private void Start()
     {
         LoadAssetToDictionaries();
-        EventManager.ActionAddHandler(EVENT.RoundBegin, StartMatch);
+        EventManager.ActionAddHandler(EVENT.RoundBegin, LoadGame);
     }
 }
