9ffad6d265d98d8363acc449909b477b9f411dfd
Ritvanen Aapo TTV17SA <aaporitvanen@kamk.fi>
2018-07-03 14:55:27 +0300

    ADD network branch
    Includes:
     - WIP network player with movement updates
     - WIP lobby

---
+++ Assets/Scripts/Network/_LobbyManager.cs
@@ -0,0 +1,160 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class _LobbyManager : NetworkLobbyManager {
+
+    public static _LobbyManager Instance;
+
+    // TODO: Make a menu dictionary that is possible to edit in Unity inspector.
+    // And edit switchEnabledGameObject() to work with dictonaries
+    [SerializeField] private GameObject mainUI = null;
+    [SerializeField] private GameObject hostUI = null;
+    [SerializeField] private GameObject clientUI = null;
+
+    [SerializeField] private Text hostingText = null;
+    [SerializeField] private Text clientAddressText = null;
+
+    // Used in other scripts
+    [SerializeField] public GameObject playerListContent = null;
+    [SerializeField] public InputField playerName = null;
+
+    [SerializeField] private string hostUIMessage = "Hosting match in\n";
+
+    private bool hosting = false;
+
+    private void Awake ()
+    {
+        Instance = this;
+    }
+
+    private void OnEnable()
+    {
+        switchEnabledGameObject(new GameObject[] { mainUI, hostUI, clientUI }, 0);
+    }
+
+    // Each Pressed method is used in UI buttons (Button in Unity Editor -> OnClick())
+
+    public void HostGamePressed()
+    {
+        if (!isNetworkActive)
+        {
+            if (playerName.text == "")
+            {
+                Debug.Log("_LobbyManager, HostGamePressed(): Insert name!");
+            }
+            else
+            {
+                // Called from NetworkManager. NetworkManager calls then OnLobbyStartHost()
+                StartHost();
+            }
+        }
+    }
+
+    public void EndHostedGamePressed()
+    {
+        if (isNetworkActive)
+        {
+            // OnLobbyStopHost()
+            StopHost();
+        }
+    }
+
+    public void JoinGamePressed()
+    {
+        if (playerName.text == "")
+        {
+            Debug.Log("_LobbyManager, HostGamePressed(): Insert name!");
+        }
+        else
+        {
+            // OnLobbyClientEnter()
+            StartClient();
+        }
+    }
+
+    public void DisconnectGamePressed()
+    {
+        if (isNetworkActive)
+        {
+            // OnLobbyClientExit()
+            StopClient();
+        }
+    }
+
+    // InputField update methods
+
+    public void IPChanged(string IP)
+    {
+        networkAddress = IP;
+    }
+
+    public void PortChanged(string port)
+    {
+        int parsedPort = 0;
+        if (int.TryParse(port, out parsedPort))
+        {
+            networkPort = parsedPort;
+        }
+    }
+
+    // Lobby network methods
+
+    public override void OnLobbyStartHost()
+    {
+        base.OnLobbyStartHost();
+
+        hosting = true;
+        hostingText.text = hostUIMessage + networkAddress + ":" + networkPort;
+        switchEnabledGameObject(new GameObject[] {mainUI, hostUI, clientUI}, 1);
+        Debug.Log("Hosting started...");
+    }
+
+    public override void OnLobbyStopHost()
+    {
+        base.OnLobbyStopHost();
+
+        hosting = false;
+        switchEnabledGameObject(new GameObject[] { mainUI, hostUI, clientUI }, 0);
+        Debug.Log("Hosting stopped.");
+    }
+
+    public override void OnLobbyClientEnter()
+    {
+        base.OnLobbyClientEnter();
+
+        if (!hosting)
+        {
+            clientAddressText.text = networkAddress + ":" + networkPort;
+            switchEnabledGameObject(new GameObject[] { mainUI, hostUI, clientUI }, 2);
+        }
+        Debug.Log("Client joined!");
+    }
+
+    public override void OnLobbyClientExit()
+    {
+        base.OnLobbyClientExit();
+
+        if (!hosting)
+        {
+            switchEnabledGameObject(new GameObject[] { mainUI, hostUI, clientUI }, 0);
+        }
+        Debug.Log("Client exited.");
+    }
+
+    // Enables only one of the objects in GameObject[] and disables others (NEEDS REPLACEMENT! See the top TODO)
+    private void switchEnabledGameObject(GameObject[] obj, int index)
+    {
+        for (int o = 0; o < obj.Length; o++)
+        {
+            if (o == index)
+            {
+                obj[o].SetActive(true);
+            }
+            else if (obj[o].activeSelf)
+            {
+                obj[o].SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Network/_LobbyPlayer.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class _LobbyPlayer : NetworkLobbyPlayer {
+
+    // Text component in child object
+    [SerializeField] private Text playerNameText = null;
+
+    [SyncVar(hook = "changePlayerName")]
+    private string playerName = "";
+
+    public override void OnClientEnterLobby()
+    {
+        base.OnClientEnterLobby();
+
+        if (isServer)
+            Debug.Log("Server");
+        if (isClient)
+            Debug.Log("Client");
+
+        CmdChangePlayerName(_LobbyManager.Instance.playerName.text);
+
+        transform.SetParent(_LobbyManager.Instance.playerListContent.transform, false);
+    }
+
+    public override void OnClientExitLobby()
+    {
+        base.OnClientExitLobby();
+    }
+
+    private void changePlayerName(string name)
+    {
+        Debug.Log("Client name: " + name);
+        playerName = name;
+        playerNameText.text = name;
+    }
+
+    [Command]
+    private void CmdChangePlayerName(string name)
+    {
+        Debug.Log("Server name: " + name);
+        playerName = name;
+    }
+}
