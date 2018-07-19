9e9d13e8c4105ce2364c73931e78de4cff9b0a73
ViljaKettu <31925883+ViljaKettu@users.noreply.github.com>
2018-07-19 10:31:44 +0300

    Start for camera collision

---
+++ Assets/Scripts/Character/CameraCollisionChecker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollisionChecker : MonoBehaviour
+{
+    //Cast ray from camera to player to check if anything in between or if camera colliding
+    //Find near clip plane points of camera (left-up, right-up, left-down, right-down, camera-position)
+
+    private void CameraCollision()
+    {
+
+    }
+
+    private void HandlePivotPosition()
+    {
+
+    }
+
+    //Move camera closer if something found
+    //Move camera to limit shearing
+
+    //Fade character if camera too close
+
+}
