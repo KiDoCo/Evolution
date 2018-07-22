b65dc05fbdba1603afbe4ebdd0351974ead98562
Apetzu <aapo.ritvanen@gmail.com>
2018-07-23 00:41:33 +0300

    FIX project files and errors

--- Assets/Scripts/Network/NetworkPlayer.cs
+++ Assets/Scripts/Network/NetworkPlayer.cs
@@ -38,8 +38,8 @@ public class NetworkPlayer : NetworkBehaviour {
     {
         if (isLocalPlayer)
         {
-            GameObject camera = Instantiate(playerCamera);
-            camera.GetComponent<CameraMovement>().FollowedPlayer = this.gameObject;
+            // Spawn camera
+			
             UIManager.Instance.InstantiateInGameUI();
         }
     }
