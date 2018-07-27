861c0c60393fcc056b630e5b77c19e35f902f16d
ramitammela2 <37778027+ramitammela2@users.noreply.github.com>
2018-07-27 12:56:25 +0300

    ADD caustics and underwater shaders

---
+++ Assets/Plugins/Projectors/Ocean/Scripts/MaterialOffset.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialOffset : MonoBehaviour
+{
+    public int materialIndex = 0;
+
+    public string textureName = "_MainTex";
+    public Vector2 uvAnimationRate = new Vector2(1.0f, 0.0f);
+    public bool moveTexture;
+
+    public string normalName = "_BumpMap";
+    public Vector2 uvAnimationRateNormal = new Vector2(1.0f, 0.0f);
+    public bool moveNormal;
+
+
+    public float amplitude;
+    public float frequency;
+
+    public Renderer renderer;
+
+    Vector2 uvOffset = Vector2.zero;
+    Vector2 uvOffsetNormal = Vector2.zero;
+
+
+    void LateUpdate()
+    {
+        uvOffset += (uvAnimationRate * Time.deltaTime * (Mathf.Sin(2 * Mathf.PI * frequency * Time.time) - Mathf.Sin(2 * Mathf.PI * frequency * (Time.time - Time.deltaTime))));
+        uvOffsetNormal += (uvAnimationRateNormal * Time.deltaTime * (Mathf.Sin(2 * Mathf.PI * frequency * Time.time) - Mathf.Sin(2 * Mathf.PI * frequency * (Time.time - Time.deltaTime))));
+
+
+        //transform.position += amplitude * (Mathf.Sin(2 * Mathf.PI * frequency * Time.time) - Mathf.Sin(2 * Mathf.PI * frequency * (Time.time - Time.deltaTime))) * transform.forward;
+
+        if (renderer.enabled)
+        {
+            if (moveTexture)
+            {
+                renderer.materials[materialIndex].SetTextureOffset(textureName, uvOffset);
+            }
+            if (moveNormal)
+            {
+                renderer.materials[materialIndex].SetTextureOffset(normalName, uvOffsetNormal);
+            }
+
+        }
+    }
+}
---
+++ Assets/Plugins/Projectors/Shadow/ProjectorIntensity.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectorIntensity : MonoBehaviour
+{
+    public Projector projector;
+        [Range(0.1f, 5)]
+        public float mininumSize = 0.35f;
+        [Range(0.1f, 5)]
+        public float maximumSize = 0.7f;
+
+    public float currentTerrainHeight;
+
+
+	void Start ()
+    {
+        if (projector == null)
+
+        projector = transform.GetComponent<Projector>();
+	}
+	
+	void LateUpdate ()
+    {
+        //Get current Terrain + height of it at current position
+        Vector3 pos = transform.position;
+        pos.y = Terrain.activeTerrain.SampleHeight(transform.position);
+
+        projector.orthographicSize = currentTerrainHeight / 2f;
+
+        currentTerrainHeight = pos.y;
+
+        //Limit projector size
+        projector.orthographicSize = Mathf.Clamp(projector.orthographicSize, mininumSize, maximumSize);
+    }
+}
